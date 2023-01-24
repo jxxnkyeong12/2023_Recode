21ce24b762494d6074bdbd593b354b7a38670315
진경 <znzlzn651@gmail.com>
2023-01-24 22:23:52 +0900

    2023-01-24 프로그래머스 mysql 레벨1 완료

--- DB/mysql/프로그래머스/Lv.1/문제풀이.sql
+++ DB/mysql/프로그래머스/Lv.1/문제풀이.sql
@@ -145,3 +145,44 @@ SELECT MAX(DATETIME) AS 시간
 FROM ANIMAL_INS 
 
 – datetime도 max가 되는구나
+
+
+
+10. 조건에 맞는 도서 리스트 출력하기
+BOOK 테이블에서 2021년에 출판된 '인문' 카테고리에 속하는 도서 리스트를 찾아서 도서 ID(BOOK_ID), 출판일 (PUBLISHED_DATE)을 출력하는 SQL문을 작성해주세요.
+결과는 출판일을 기준으로 오름차순 정렬해주세요.
+
+
+SELECT BOOK_ID, DATE_FORMAT( PUBLISHED_DATE , '%Y-%m-%d') AS PUBLISHED_DATE
+FROM BOOK
+WHERE CATEGORY LIKE '인문'
+AND PUBLISHED_DATE  LIKE '2021%'
+ORDER BY PUBLISHED_DATE ASC
+
+
+
+
+
+
+
+11. 자동차 대여 기록에서 장기/단기 대여 구분하기
+CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 대여 시작일이 2022년 9월에 속하는 대여 기록에 대해서 대여 기간이 30일 이상이면 '장기 대여' 그렇지 않으면 '단기 대여' 로 표시하는 컬럼(컬럼명: RENT_TYPE)을 추가하여 대여기록을 출력하는 SQL문을 작성해주세요. 결과는 대여 기록 ID를 기준으로 내림차순 정렬해주세요.
+
+- 두 날짜간의 차이 가져오기 함수
+: 단순히 일 차이를 가져올 때 사용하는 것이 DATEDIFF 함수.
+ 이 외에도 차이를 연, 분기, 월, 주, 일, 시, 분, 초를 지정하여 가져올 때 사용하는 TIMESTAMPDIFF 함수입니다.
+
+
+SELECT  HISTORY_ID
+        , CAR_ID
+        , DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE
+        , DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE
+        , CASE 
+            WHEN  DATEDIFF(END_DATE, START_DATE) + 1 >= 30
+            THEN "장기 대여"
+            ELSE "단기 대여"
+          END AS RENT_TYPE
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+WHERE START_DATE LIKE ('2022-09%')
+ORDER BY HISTORY_ID DESC;
+
